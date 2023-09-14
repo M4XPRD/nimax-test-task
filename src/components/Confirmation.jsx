bae47d26d886298e6d7bdec38eaa30f36559f5b9
Max <ppmax@bk.ru>
2023-09-14 19:40:12 +0300

    Page 2 finished

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import useForm from '../hooks/formHook';
+import countPrice from '../utils/countPrice';
+import { pluralizeNights, pluralizeVisitors } from '../utils/pluralizeData';
 
-const Confirmation = () => (
-  <div>Confirmation</div>
-);
+const Confirmation = ({ f }) => {
+  const { handleNextPage, handlePreviousPage } = useForm();
+
+  return (
+    <div className="form-container">
+      <div className="form-inputs">
+
+        <div className="form-control">
+          <p>{`${f.values.surname} ${f.values.name} ${f.values.middleName}`}</p>
+          <p>{f.values.phoneNumber}</p>
+          <p>{pluralizeNights(f)}</p>
+          <p>{pluralizeVisitors(f)}</p>
+          <p>{`К оплате ${countPrice(f)} ₽`}</p>
+        </div>
+
+        <div className="form-results">
+
+          <div className="form-next-button">
+            <button
+              type="button"
+              className="next-button"
+              onClick={() => handleNextPage()}
+            >
+              Далее
+            </button>
+          </div>
+
+          <div className="form-return-button">
+            <button
+              type="button"
+              className="return-button"
+              onClick={() => handlePreviousPage()}
+            >
+              Назад к данным покупателя
+            </button>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  );
+};
 
 export default Confirmation;

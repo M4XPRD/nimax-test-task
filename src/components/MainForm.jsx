74fff1344eada8d888e20639642620cf6819749d
Max <ppmax@bk.ru>
2023-09-14 20:52:00 +0300

    Formatting fixes

--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -8,28 +8,49 @@ import Confirmation from './Confirmation';
 import PaymentSuccess from './PaymentSuccess';
 
 const MainForm = () => {
-  const { page, pageTitle } = useForm();
+  const { page, pageTitle, handleNextPage } = useForm();
 
   const validationSchema = yup.object().shape({
-    adultsAmount: yup.number().min(1, 'Должен быть минимум 1 взрослый'),
+    adultsAmount: yup
+      .number()
+      .min(1, 'Должен быть минимум 1 взрослый')
+      .required('Это поле обязательно'),
     children5To12: yup.number(),
-    childrenBelow5: yup.number().test(
-      'acceptedAmount',
-      'Допустимо максимум 3 ребёнка на 1 взрослого',
-      (children, { parent: { adultsAmount } }) => {
-        if (adultsAmount > 0 && children > 0) {
-          const acceptedRatio = 0.33;
-          const currentRatio = (adultsAmount / children).toFixed(2);
-          return currentRatio === Infinity ? true : currentRatio >= acceptedRatio;
-        }
-        return true;
-      },
-    ),
-    nightsAmount: yup.number().min(1, 'Нужно остановиться минимум на 1 ночь'),
-    surname: yup.string().matches(/^[^\d]*$/, 'Поле не должно содержать цифры').required('Это поле обязательно'),
-    name: yup.string().matches(/^[^\d]*$/, 'Поле не должно содержать цифры').required('Это поле обязательно'),
-    middleName: yup.string().matches(/^[^\d]*$/, 'Поле не должно содержать цифры'),
-    phoneNumber: yup.string().min(8, 'Проверьте количество символов в номере').required('Это поле обязательно'),
+    childrenBelow5: yup
+      .number()
+      .test(
+        'acceptedAmount',
+        'Допустимо максимум 3 ребёнка на 1 взрослого',
+        (children, { parent: { adultsAmount } }) => {
+          if (adultsAmount > 0 && children > 0) {
+            const acceptedRatio = 0.33;
+            const currentRatio = (adultsAmount / children).toFixed(2);
+            return currentRatio === Infinity
+              ? true
+              : currentRatio >= acceptedRatio;
+          }
+          return true;
+        },
+      ),
+    nightsAmount: yup
+      .number()
+      .min(1, 'Нужно остановиться минимум на 1 ночь')
+      .required('Это поле обязательно'),
+    surname: yup
+      .string()
+      .matches(/^[^\d]*$/, 'Поле не должно содержать цифры')
+      .required('Это поле обязательно'),
+    name: yup
+      .string()
+      .matches(/^[^\d]*$/, 'Поле не должно содержать цифры')
+      .required('Это поле обязательно'),
+    middleName: yup
+      .string()
+      .matches(/^[^\d]*$/, 'Поле не должно содержать цифры'),
+    phoneNumber: yup
+      .string()
+      .min(8, 'Проверьте количество символов в номере')
+      .required('Это поле обязательно'),
     birthDate: yup.string().required('Это поле обязательно'),
   });
 
@@ -51,8 +72,10 @@ const MainForm = () => {
     validationSchema,
     validateOnBlur: true,
     validateOnChange: true,
-    onSubmit: async (e) => {
-      e.preventDefault();
+    onSubmit: async () => {
+      console.log(f.values.finalSum);
+      handleNextPage();
+      f.resetForm();
     },
   });
 

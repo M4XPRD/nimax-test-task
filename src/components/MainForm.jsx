745bbd310144903c6f53ee9c02fb44a750d67ea4
Max <ppmax@bk.ru>
2023-09-15 02:48:15 +0300

    Validation schema moved to utils

--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as yup from 'yup';
 import { useFormik } from 'formik';
 import useForm from '../hooks/formHook';
 import CostCalc from './CostCalc';
@@ -7,59 +6,13 @@ import BuyersDetails from './BuyersDetails';
 import Confirmation from './Confirmation';
 import PaymentSuccess from './PaymentSuccess';
 import useNetwork from '../hooks/networkHook';
+import validationSchema from '../utils/validation/validationSchema';
 
 const MainForm = () => {
   const {
     page, pageTitle, handleNextPage,
   } = useForm();
   const { setNetworkError, setLoading } = useNetwork();
-
-  const validationSchema = yup.object().shape({
-    adultsAmount: yup
-      .number()
-      .min(1, 'Должен быть минимум 1 взрослый')
-      .required('Это поле обязательно'),
-    children5To12: yup
-      .number(),
-    childrenBelow5: yup
-      .number()
-      .test(
-        'acceptedAmount',
-        'Допустимо максимум 3 ребёнка на 1 взрослого',
-        (children, { parent: { adultsAmount } }) => {
-          if (adultsAmount > 0 && children > 0) {
-            const acceptedRatio = 0.33;
-            const currentRatio = (adultsAmount / children).toFixed(2);
-            return currentRatio === Infinity
-              ? true
-              : currentRatio >= acceptedRatio;
-          }
-          return true;
-        },
-      ),
-    nightsAmount: yup
-      .number()
-      .min(1, 'Нужно остановиться минимум на 1 ночь')
-      .required('Это поле обязательно'),
-    surname: yup
-      .string()
-      .matches(/^[^\d]*$/, 'Поле не должно содержать цифры')
-      .required('Это поле обязательно'),
-    name: yup
-      .string()
-      .matches(/^[^\d]*$/, 'Поле не должно содержать цифры')
-      .required('Это поле обязательно'),
-    middleName: yup
-      .string()
-      .matches(/^[^\d]*$/, 'Поле не должно содержать цифры'),
-    phoneNumber: yup
-      .string()
-      .min(8, 'Проверьте количество символов в номере')
-      .required('Это поле обязательно'),
-    birthDate: yup
-      .string()
-      .required('Это поле обязательно'),
-  });
 
   const f = useFormik({
     initialValues: {
@@ -80,7 +33,6 @@ const MainForm = () => {
     validateOnBlur: true,
     validateOnChange: false,
     onSubmit: async () => {
-      setLoading(false);
       setNetworkError(false);
       try {
         setLoading(true);

7575ae99d8cd55804dd152d34d2aec461c789f5a
Max <ppmax@bk.ru>
2023-09-14 20:51:28 +0300

    Last page created

--- src/components/PaymentSuccess.jsx
+++ src/components/PaymentSuccess.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import useForm from '../hooks/formHook';
+import success from '../assets/page-3-success.svg';
 
-const PaymentSuccess = () => (
-  <div>PaymentSuccess</div>
-);
+const PaymentSuccess = () => {
+  const { handleRestartForm } = useForm();
+  return (
+    <div className="form-container">
+      <div className="form-inputs">
+
+        <div className="form-control">
+          <img src={success} alt="Заказ оплачен" />
+          <p>Заказ успешно оплачен.</p>
+        </div>
+
+        <div className="form-results">
+          <div className="form-next-button">
+            <button
+              type="button"
+              className="next-button"
+              onClick={() => handleRestartForm()}
+            >
+              Далее
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
 
 export default PaymentSuccess;

b130989055be268f58738c3334f3d59df0bdf920
Max <ppmax@bk.ru>
2023-09-16 20:46:44 +0300

    RoomType edited with more containers

--- src/components/CostCalc/RoomType.jsx
+++ src/components/CostCalc/RoomType.jsx
@@ -6,7 +6,7 @@ const RoomType = (props) => {
   } = props;
 
   return (
-    <div className="form-control">
+    <div className={mode === 'mobile' ? 'form-control' : 'form-control-desktop'}>
       {mode === 'mobile' ? (
         <label htmlFor="roomType">
           Тип номера
@@ -23,41 +23,43 @@ const RoomType = (props) => {
           </select>
         </label>
       ) : (
-        <div>
-          Тип номера
-          <label htmlFor="roomType">
-            <input
-              name="roomType"
-              id="roomType"
-              type="radio"
-              value="Эконом"
-              checked={value === 'Эконом'}
-              onChange={handleChange}
-            />
-            Эконом
-          </label>
-          <label htmlFor="roomType">
-            <input
-              name="roomType"
-              id="roomType"
-              type="radio"
-              value="Стандарт"
-              checked={value === 'Стандарт'}
-              onChange={handleChange}
-            />
-            Стандарт
-          </label>
-          <label htmlFor="roomType">
-            <input
-              name="roomType"
-              id="roomType"
-              type="radio"
-              value="Люкс"
-              checked={value === 'Люкс'}
-              onChange={handleChange}
-            />
-            Люкс
-          </label>
+        <div className="roomtype-container">
+          <div>Тип номера</div>
+          <div className="radio-container">
+            <label htmlFor="roomType">
+              <input
+                name="roomType"
+                id="roomType"
+                type="radio"
+                value="Эконом"
+                checked={value === 'Эконом'}
+                onChange={handleChange}
+              />
+              Эконом
+            </label>
+            <label htmlFor="roomType">
+              <input
+                name="roomType"
+                id="roomType"
+                type="radio"
+                value="Стандарт"
+                checked={value === 'Стандарт'}
+                onChange={handleChange}
+              />
+              Стандарт
+            </label>
+            <label htmlFor="roomType">
+              <input
+                name="roomType"
+                id="roomType"
+                type="radio"
+                value="Люкс"
+                checked={value === 'Люкс'}
+                onChange={handleChange}
+              />
+              Люкс
+            </label>
+          </div>
         </div>
       )}
     </div>

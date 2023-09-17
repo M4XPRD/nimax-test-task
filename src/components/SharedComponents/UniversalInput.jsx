ad91c3104e5e6497d425a353dc7da01fde690593
Max <ppmax@bk.ru>
2023-09-17 03:17:06 +0300

    Phone number regex fixed

--- src/components/SharedComponents/UniversalInput.jsx
+++ src/components/SharedComponents/UniversalInput.jsx
@@ -28,7 +28,9 @@ const UniversalInput = (props) => {
           onBlur={handleBlur}
           onChange={type === 'tel' ? (e) => {
             const inputValue = e.target.value;
-            const numericInputValue = inputValue.replace(/[^0-9+\s-]/g, '');
+            const numericInputValue = inputValue
+              .replace(/[^0-9+\s-]/g, '')
+              .replace(/[^+\d]/g, '').replace(/(\d{2})(?=\d{2}$)/, '$1-');
             handleChange({
               target: {
                 name: 'phoneNumber',
@@ -36,7 +38,7 @@ const UniversalInput = (props) => {
               },
             });
           } : handleChange}
-          pattern={type === 'tel' ? '+7 d{3} d{3} d{2}-d{2}' : undefined}
+          pattern={type === 'tel' ? '\\+7\\s\\d{3}\\s\\d{3}\\s\\d{2}-\\d{2}' : undefined}
           inputMode={type === 'tel' ? 'numeric' : undefined}
           placeholder={type === 'tel' ? '+ 7 999 123 45-67' : undefined}
           max={type === 'date' ? '9999-12-31' : undefined}
